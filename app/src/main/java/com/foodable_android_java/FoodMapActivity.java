31502687638c8017727e851f1442a808df8105a7
Hunaid2000 <hunaid2000@gmail.com>
2022-12-07 22:29:08 +0500

    Implemented markers for food map

--- app/src/main/java/com/foodable_android_java/FoodMapActivity.java
+++ app/src/main/java/com/foodable_android_java/FoodMapActivity.java
@@ -3,13 +3,17 @@ package com.foodable_android_java;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.location.CurrentLocationRequest;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.Granularity;
@@ -20,12 +24,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.foodable_android_java.databinding.ActivityLocationBinding;
 import com.google.android.gms.tasks.CancellationTokenSource;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.maps.android.SphericalUtil;
+
+import java.util.ArrayList;
 
 
 public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -33,6 +40,8 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
     private FusedLocationProviderClient fusedLocationClient;
     LatLng currentLocation;
     ImageButton backButtton, myLocation;
+    Marker currentMarker;
+    ArrayList<Donation> donations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,8 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
         myLocation = findViewById(R.id.my_location);
         backButtton = findViewById(R.id.back_button);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        donations = new ArrayList<>();
+        getAllDonations();
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 100);
@@ -62,7 +73,7 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
                     CurrentLocationRequest request = new CurrentLocationRequest.Builder()
                             .setGranularity(Granularity.GRANULARITY_FINE)
                             .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
-                            .setDurationMillis(2000)
+                            .setDurationMillis(4000)
                             .setMaxUpdateAgeMillis(0)
                             .build();
 
@@ -72,7 +83,7 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
                                 if (location != null) {
                                     currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
                                     mMap.clear();
-                                    mMap.addMarker(new MarkerOptions().position(currentLocation).title("My Location"));
+                                    currentMarker = mMap.addMarker(new MarkerOptions().position(currentLocation).title("My Location"));
                                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14));
                                     getOtherDonations();
                                 }
@@ -100,7 +111,7 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
                 .addOnSuccessListener(this, location -> {
                     if (location != null) {
                         currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Last Location"));
+                        currentMarker = mMap.addMarker(new MarkerOptions().position(currentLocation).title("Last Location"));
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14));
                     }
                 });
@@ -116,13 +127,61 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
             public void onMapClick(LatLng point) {
                 mMap.clear();
                 currentLocation = new LatLng(point.latitude, point.longitude);
-                mMap.addMarker(new MarkerOptions().position(currentLocation).title("New Marker"));
+                currentMarker =  mMap.addMarker(new MarkerOptions().position(currentLocation).title("New Marker"));
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14));
                 getOtherDonations();
             }
         });
 
         getOtherDonations();
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                if (!marker.equals(currentMarker)) {
+                    View v = getLayoutInflater().inflate(R.layout.marker_infowindow, null);
+                    TextView userName = v.findViewById(R.id.userName);
+                    ImageView profile = v.findViewById(R.id.userImg);
+                    TextView itemName = v.findViewById(R.id.itemName);
+                    ImageView itemImg = v.findViewById(R.id.itemImg);
+                    TextView distance = v.findViewById(R.id.distance);
+
+                    for (Donation donation : donations) {
+                        if (donation.getLocation().equals(marker.getPosition())) {
+                            userName.setText(donation.getName());
+                            itemName.setText(donation.getTitle());
+                            double dist = SphericalUtil.computeDistanceBetween(donation.getLocation(), marker.getPosition());
+                            String distanceStr = String.valueOf(dist);
+                            distanceStr = String.format("%.2f", dist / 1000) + " km";
+                            distance.setText(distanceStr);
+                            Glide.with(FoodMapActivity.this).load(donation.getImages().get(0)).into(itemImg);
+                            Glide.with(FoodMapActivity.this).load(donation.getProfile()).into(profile);
+                            break;
+                        }
+                    }
+                    return v;
+                }
+                else {
+                    return null;
+                }
+
+            }
+        });
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                if (!marker.equals(currentMarker)) {
+                    Intent intent = new Intent(FoodMapActivity.this, ReceiveActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 
@@ -131,12 +190,37 @@ public class FoodMapActivity extends FragmentActivity implements OnMapReadyCallb
             if (task.isSuccessful()) {
                 DataSnapshot dataSnapshot = task.getResult();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    for (DataSnapshot snapshot1 : snapshot.getChildren()) {
-                        String lat = snapshot1.child("location").child("latitude").getValue().toString();
-                        String lng = snapshot1.child("location").child("longitude").getValue().toString();
-                        LatLng donationLocation = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
-                        if (SphericalUtil.computeDistanceBetween(currentLocation, donationLocation) < 5000){
-                            mMap.addMarker(new MarkerOptions().position(donationLocation).title("Donation Here"));
+                    if (!snapshot.getKey().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            String lat = snapshot1.child("location").child("latitude").getValue().toString();
+                            String lng = snapshot1.child("location").child("longitude").getValue().toString();
+                            LatLng donationLocation = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+                            if (SphericalUtil.computeDistanceBetween(currentLocation, donationLocation) < 5000) {
+                                mMap.addMarker(new MarkerOptions().position(donationLocation).title("Donation Here"));
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public void getAllDonations(){
+        FirebaseDatabase.getInstance().getReference("donations").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DataSnapshot dataSnapshot = task.getResult();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (!snapshot.getKey().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            Donation donation = new Donation();
+                            donation.setTitle(snapshot1.child("title").getValue().toString());
+                            donation.setName(snapshot1.child("name").getValue().toString());
+                            donation.setProfile(snapshot1.child("profile").getValue().toString());
+                            ArrayList<String> images = new ArrayList<>();
+                            images.add(snapshot1.child("images").child("0").getValue().toString());
+                            donation.setImages(images);
+                            donation.setLocation(new LatLng(Double.parseDouble(snapshot1.child("location").child("latitude").getValue().toString()), Double.parseDouble(snapshot1.child("location").child("longitude").getValue().toString())));
+                            donations.add(donation);
                         }
                     }
                 }
